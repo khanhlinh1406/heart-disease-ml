f8af0bec936654db8bdb4fdd1ce9612163f7afc4
Linh Nguyen <72326769+khanhlinh1406@users.noreply.github.com>
2026-03-29 21:27:30 +0700

    modify: tune threshold to improve recall (class 1)

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,9 +3,12 @@ import joblib
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 def evaluate():
+    THRESHOLD = 0.4
     X_train, X_test, y_train, y_test = preprocess_data()
     model = joblib.load("model.pkl")
-    y_pred = model.predict(X_test)
+    
+    prob = model.predict_proba(X_test)[:, 1]
+    y_pred = (prob > THRESHOLD).astype(int)
 
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print("\n\nConfusion Matrix:")
--- src/predict.py
+++ src/predict.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 
 def predict(sample):
+    THRESHOLD = 0.4
     model = joblib.load("model.pkl")
     scaler = joblib.load("scaler.pkl")
 
@@ -15,7 +16,7 @@ def predict(sample):
     # predict probability
     probability = model.predict_proba(sample_scaled)
 
-    return prediction[0], probability[0]
+    return 1 if prediction[0] >= THRESHOLD else 0, probability[0]
 
 if __name__ == "__main__":
     sample = np.array([[52, 1, 0, 125, 212, 0, 1, 168, 0, 1.0, 2, 2, 3]])
